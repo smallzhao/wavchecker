bb26aba6fedd956b61906b0c89b1e85e8e312dff
zhaowenhua <zhaowenhua@datatang.com>
2019-09-22 21:59:15 +0800

    fix

--- plugins/audiofilters/src/main.py
+++ plugins/audiofilters/src/main.py
@@ -10,25 +10,25 @@ def main(input, result, args, taskinfos=None):
 
 
 if __name__ == '__main__':
-    # taskinfos = os.environ.get('taskinfos')
-    # args = os.environ.get('args')
-    #
-    # main('/input', '/result', args, taskinfos=taskinfos)
+    taskinfos = os.environ.get('taskinfos')
+    args = os.environ.get('args')
+
+    main('/input', '/result', args, taskinfos=taskinfos)
 
     # while 1:
     #     import time
     #     time.sleep(3)
 
-    input = r'C:\Users\Aorus\Desktop\test\测试'
-    output = r'C:\Users\Aorus\Desktop\test\测试'
-    args = 'energylost@disturb_detect'
+    # input = r'C:\Users\Aorus\Desktop\test\测试'
+    # output = r'C:\Users\Aorus\Desktop\test\测试'
+    # args = 'energylost@disturb_detect'
 
-    groups = []
+    # groups = []
     # with open(r'C:\Users\Aorus\Desktop\test.csv', 'r') as f:
     #     for line in f.readlines():
     #         task_id, group = line.split(',')
     #         groups.append((task_id.strip(), group.strip()))
     # taskinfos = base64.b64encode(json.dumps(groups).encode())
-    taskinfos = ''
+    # taskinfos = ''
 
-    main(input, output, args, taskinfos=taskinfos)
+    # main(input, output, args, taskinfos=taskinfos)
